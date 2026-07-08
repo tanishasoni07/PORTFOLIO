75a9cfef541315d6826034648d85759ffa1afa81
tanishasoni07 <sonitanisha0107@gmail.com>
2026-07-09 00:38:55 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+ import { useEffect, useState } from 'react'
 import AOS from'aos'
 import 'aos/dist/aos.css'
 import Header from'./components/Header'
